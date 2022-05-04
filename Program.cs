194f6c075cecf761f7c400922bdd5e3bf6f027f2
8778saquib <saquib.nawaz009@gmail.com>
2022-05-04 14:53:01 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace emp_wage1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee wage camputation");
+        }
+    }
+}
